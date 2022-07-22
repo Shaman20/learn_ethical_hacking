f5309819214c0c48e06f187ceafa12ca0cdcd25f
Shaman20 <shamansr21@gmail.com>
2022-07-22 12:46:27 +0530

    added_user_input

--- change_mac_adress.py
+++ change_mac_adress.py
@@ -4,12 +4,17 @@
 import subprocess
 
 # Assigning variables for my interface
-interface = 'eth0'
-new_mac = '00:AA:11:BB:22:CC'
+interface = input('Enter the interface name: ')
+
+new_mac = input('Enter the new mac-address: ')
 
 print(f'Changing the mac address {interface} to new_mac')
 
-subprocess.call(f'sudo ifconfig {interface} down', shell=True)
-subprocess.call(f'sudo ifconfig eth0 hw ether {new_mac}' ,shell=True)
-subprocess.call(f'sudo ifconfig {interface} up', shell=True)
+subprocess.call(['sudo','ifconfig', interface, 'down'])
+subprocess.call(['sudo', 'ifconfig', interface, 'hw', 'ether', new_mac])
+subprocess.call(['sudo', 'ifconfig', interface, 'up'])
+
+print(f'The Changed MAC-Address is -->')
+val = subprocess.call(['sudo', 'ifconfig'])
+print(val)
 
